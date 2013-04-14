f9a4a923e6cd2fddf1fd128f07d498b931b2714d
Joe Kington <joferkington@gmail.com>
2013-04-14 14:23:31 -0500

    Minor clarification in change_popup_color example

--- examples/change_popup_color.py
+++ examples/change_popup_color.py
@@ -5,10 +5,11 @@ from mpldatacursor import datacursor
 fig, axes = plt.subplots(ncols=2)
 
 left_artist = axes[0].plot(range(10))
-axes[0].set(title='Left datacursor will\nnot have a background')
+axes[0].set(title='Left datacursor will be to the lower'
+                  '\nright and not have a background')
 
 right_artist = axes[1].imshow(np.arange(100).reshape(10,10))
-axes[1].set(title='Right datacursor will\nhave a white background')
+axes[1].set(title='Right datacursor will have\na fancy white background')
 
 # Make the text pop up "underneath" the line and remove the box...
 dc1 = datacursor(left_artist, xytext=(15, -15), bbox=None)
@@ -16,6 +17,6 @@ dc1 = datacursor(left_artist, xytext=(15, -15), bbox=None)
 # Make the box have a white background with a fancier connecting arrow
 dc2 = datacursor(right_artist, 
              arrowprops=dict(arrowstyle='simple', fc='white', alpha=0.5),
-             bbox=dict(boxstyle='round,pad=0.5', fc='white', alpha=0.5))
+             bbox=dict(boxstyle='round,pad=0.3', fc='white', alpha=0.5))
 
 plt.show()
