42a1dbb8a17e21191e0f60f7e60be4e5761f33e6
Joe Kington <joferkington@gmail.com>
2013-05-19 10:01:33 -0500

    Incremented version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'mpldatacursor',
-    version = '0.2-dev',
+    version = '0.3',
     description = "Interactive data cursors for Matplotlib",
     author = 'Joe Kington',
     author_email = 'joferkington@gmail.com',
