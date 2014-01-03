dac9d1f72dcc29f83f70f981d2a9320cbd74c42c
Joe Kington <joferkington@gmail.com>
2014-01-03 16:45:21 -0600

    Improved hover example

--- examples/hover_example.py
+++ examples/hover_example.py
@@ -1,15 +1,23 @@
-"""A very basic exmaple of the hover functionality of mpldatacursor."""
+"""
+Demonstrates the hover functionality of mpldatacursor as well as point labels
+and a custom formatting function. Notice that overlapping points have both 
+labels displayed.
+"""
+import string
 import matplotlib.pyplot as plt
 import numpy as np
 from mpldatacursor import datacursor
+np.random.seed(1977)
 
-x, y = np.random.random((2, 6))
-labels = ['a', 'b', 'c', 'd', 'e', 'f']
+x, y = np.random.random((2, 26))
+labels = string.ascii_lowercase
 
 fig, ax = plt.subplots()
 ax.scatter(x, y, s=200)
 ax.set_title('Mouse over a point')
 
-datacursor(hover=True, point_labels=labels)
+# Show only the point label and allow nicer formatting if points overlap
+formatter = lambda **kwargs: ', '.join(kwargs['point_label'])
+datacursor(hover=True, formatter=formatter, point_labels=labels)
 
 plt.show()
