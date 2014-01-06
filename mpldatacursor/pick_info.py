f1f6e3e376b1ef09f175da69431aaf4849f08da4
Joe Kington <joferkington@gmail.com>
2014-01-05 22:32:20 -0600

    Changed copyright assignment to mpldatacursor developers, as no copyright transfer form to FSF had been filled out

--- mpldatacursor/pick_info.py
+++ mpldatacursor/pick_info.py
@@ -1,5 +1,5 @@
 __license__ = """
-Copyright (c) 2012 Free Software Foundation
+Copyright (c) 2012 mpldatacursor developers
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
