abccf4082596d3e3d05b032001bee8a6f15b7a4c
Joe Kington <joferkington@gmail.com>
2015-08-16 16:28:58 -0500

    Incremented version number

--- mpldatacursor/__init__.py
+++ mpldatacursor/__init__.py
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-__version__ = '0.5-dev'
+__version__ = '0.6-dev'
 
 from .convenience import datacursor
 from .datacursor import DataCursor, HighlightingDataCursor
