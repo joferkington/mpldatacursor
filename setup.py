a9dd323d41caf04f06ec2b357ad334ba868f052a
Joe Kington <joe.kington@planet.com>
2019-12-31 11:27:31 -0600

    Fix description

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+from os import path
 from setuptools import setup
+
+root_dir = path.abspath(path.dirname(__file__))
+with open(path.join(root_dir, 'README.rst')) as infile:
+    long_description = infile.read()
 
 setup(
     name = 'mpldatacursor',
@@ -11,5 +16,7 @@ setup(
     packages = ['mpldatacursor'],
     install_requires = [
         'matplotlib >= 0.9',
-        'numpy >= 1.1']
+        'numpy >= 1.1'],
+    long_description = long_description,
+    long_description_content_type = 'text/x-rst',
 )
